ecbbf71022f85b80b4820b5eaae3a153d21130d9
Filipe Silva <filipematossilva@gmail.com>
2021-10-28 14:30:04 +0100

    make repro

---
+++ README.md
@@ -0,0 +1,31 @@
+# fluree-query-missing-data
+
+Repro for ___
+
+Fluree will not show data for queries immediately after after the transaction.
+The data will show up a some time after though.
+
+```
+docker-compose up fluree
+# in another terminal
+clj -A:repro
+```
+
+From `src/repro.clj`:
+```
+  ;; Add an event.
+  @(fdb/transact conn ledger [(new-event 1)])
+
+  ;; The query returns [] after the transaction.
+  (println @(fdb/query (fdb/db conn ledger) {:select ["*"]
+                                             :from "event"}))
+
+  ;; But after a few seconds the query contains the event.
+  (Thread/sleep 2000)
+  (println @(fdb/query (fdb/db conn ledger) {:select ["*"]
+                                             :from "event"}))
+```
+
+If you want to re-run the repro you have to take down docker-compose, remove the folder binding via `rm -rf docker-data`, and bring it up again.
+I think this is due to to https://github.com/fluree/ledger/issues/98 because this repro also deletes the ledger.
+
---
+++ src/repro.clj
@@ -0,0 +1,68 @@
+(ns repro
+  (:require [fluree.db.api :as fdb]))
+
+
+(def ledger "events/log")
+(def conn (fdb/connect "http://localhost:8090"))
+(def schema-tx [{:_id              :_collection
+                 :_collection/name :event
+                 :_collection/doc  "Athens semantic events."}
+                {:_id               :_predicate
+                 :_predicate/name   :event/id
+                 :_predicate/doc    "A globally unique event id."
+                 :_predicate/unique true
+                 :_predicate/type   :string}
+                {:_id             :_predicate
+                 :_predicate/name :event/data
+                 :_predicate/doc  "Event data serialized as an EDN string."
+                 :_predicate/type :string}])
+
+
+(defn new-event [x]
+  {:_id        :event
+   :event/id   (str "uuid-" x)
+   :event/data "{}"})
+
+
+(def order-by-query
+  {:select {"?event" ["*"]}
+   :where  [["?event" "event/id", "?id"]]
+   ;; Subject (?event here) is a monotonically incrementing bigint,
+   ;; so ordering by that gives us insertion order.
+   :opts   {:orderBy ["ASC", "?event"]}})
+
+(defn tx-size [size]
+  (println size "bytes event data")
+  [{:_id        :event
+    :event/id   (str "size-" size)
+    :event/data (repeat size "a")}])
+
+
+(defn repro
+  []
+
+  ;; Create ledger
+  @(fdb/new-ledger conn ledger)
+  (fdb/wait-for-ledger-ready conn ledger)
+  @(fdb/transact conn ledger schema-tx)
+
+  ;; Add an event.
+  @(fdb/transact conn ledger [(new-event 1)])
+
+  ;; The query returns [] after the transaction.
+  (println @(fdb/query (fdb/db conn ledger) {:select ["*"]
+                                             :from "event"}))
+
+  ;; But after a few seconds the query contains the event.
+  (Thread/sleep 2000)
+  (println @(fdb/query (fdb/db conn ledger) {:select ["*"]
+                                             :from "event"}))
+
+  ;; Delete ledger and close conn.
+  @(fdb/delete-ledger conn ledger)
+  (fdb/close conn))
+
+(defn -main
+  []
+  (repro))
+
